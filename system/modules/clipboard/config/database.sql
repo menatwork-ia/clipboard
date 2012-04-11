9ff9d1d9b1e4e4a2c7ebe4e872b21a756768f346
Andreas Isaak <andy.jared@googlemail.com>
2012-04-11 20:23:50 +0200

    Cleanup (Step 3)

--- system/modules/clipboard/config/database.sql
+++ system/modules/clipboard/config/database.sql
@@ -12,14 +12,14 @@
 -- 
 
 CREATE TABLE `tl_clipboard` (
-        `id` int(10) unsigned NOT NULL auto_increment,
-        `user_id` int(10) unsigned NOT NULL default '0',
-        `title` varchar(128) NOT NULL default '',
-        `str_table` varchar(32) NOT NULL default '',
-        `favorite` char(1) NOT NULL default '1',
-        `childs` char(1) NOT NULL default '0',
-        `elem_id` int(10) unsigned NOT NULL default '0',
-        `filename` varchar(128) NOT NULL default '',
+    `id` int(10) unsigned NOT NULL auto_increment,
+    `user_id` int(10) unsigned NOT NULL default '0',
+    `title` varchar(128) NOT NULL default '',
+    `str_table` varchar(32) NOT NULL default '',
+    `favorite` char(1) NOT NULL default '1',
+    `childs` char(1) NOT NULL default '0',
+    `elem_id` int(10) unsigned NOT NULL default '0',
+    `filename` varchar(128) NOT NULL default '',
     PRIMARY KEY  (`id`),
     UNIQUE KEY `key` (`user_id`, `elem_id`, `str_table`, `filename`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
