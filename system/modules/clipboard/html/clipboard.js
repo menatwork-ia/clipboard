46094befd2e82159d453c8079e6ceea0efe557ce
xantippe <info@e-spin.de>
2013-11-19 14:22:22 +0100

    reset

--- system/modules/clipboard/html/clipboard.js
+++ system/modules/clipboard/html/clipboard.js
@@ -5,7 +5,7 @@
  * @copyright  MEN AT WORK 2012
  * @package    Backend
  */
-var ClipboardMenu={initialize:function(){$$("a.cl_paste").each(function(a){a.addClass("invisible")});$$("a.clipboardmenu").each(function(b){var a=b.getAllNext("a.cl_paste");a.unshift(b);b.addEvent("contextmenu",function(c){c.preventDefault();ClipboardMenu.show(a,c)})});$(document.body).addEvent("click",function(){ClipboardMenu.hide()})},show:function(a,c){ClipboardMenu.hide();var d=new Element("div",{id:"clipboardmenu",styles:{top:(a[0].getPosition().y-6)}});var b="";a.each(function(f,e){b+='<a href="'+f.href+'" title="'+f.title+'" onclick="Backend.getScrollOffset();">'+f.get("html")+" "+f.getFirst("img").alt+"</a>"});d.set("html",b);d.inject($(document.body));d.setStyle("left",a[0].getPosition().x-(d.getSize().x/2))},hide:function(){if($defined($("clipboardmenu"))){$("clipboardmenu").destroy()}}};
+var ClipboardMenu={initialize:function(){$$("a.cl_paste").each(function(a){a.addClass("invisible")});$$("a.clipboardmenu").each(function(b){var a=b.getAllNext("a.cl_paste");a.unshift(b);b.addEvent("contextmenu",function(c){c.preventDefault();ClipboardMenu.show(a,c)})});$(document.body).addEvent("click",function(){ClipboardMenu.hide()})},show:function(a,c){ClipboardMenu.hide();var d=new Element("div",{id:"clipboardmenu",styles:{top:(a[0].getPosition().y-6)}});var b="";a.each(function(f,e){b+='<a href="'+f.href+'" title="'+f.title+'" onclick="Backend.getScrollOffset();">'+f.get("html")+" "+f.getFirst("img").alt+"</a>"});d.set("html",b);d.inject($(document.body));d.setStyle("left",a[0].getPosition().x-(d.getSize().x/2))},hide:function(){if($("clipboardmenu")!=null){$("clipboardmenu").destroy()}}};
 
 /**
  * Class Clipboard
