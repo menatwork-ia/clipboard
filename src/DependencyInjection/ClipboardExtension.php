5f73796c397dc01f074c4a3269713ef69d4c7574
Stefan Heimes <stefan_heimes@hotmail.com>
2019-10-12 17:06:07 +0200

    Activate and rename the services yml.

--- src/DependencyInjection/ClipboardExtension.php
+++ src/DependencyInjection/ClipboardExtension.php
@@ -29,7 +29,7 @@ class ClipboardExtension extends ConfigurableExtension
     private $files = [
 //        'commands.yml',
 //        'listener.yml',
-//        'services.yml',
+        'services.yml'
     ];
 
     /**
