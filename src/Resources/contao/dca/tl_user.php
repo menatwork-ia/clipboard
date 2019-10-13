02a7fe9814968328fa6caeb3482db1dd728b21a7
Stefan Heimes <stefan_heimes@hotmail.com>
2019-10-13 14:28:25 +0200

    Remove database.sql and move all to the dca.

--- src/Resources/contao/dca/tl_user.php
+++ src/Resources/contao/dca/tl_user.php
@@ -3,40 +3,39 @@
 /**
  * Contao Open Source CMS
  *
- * @copyright  MEN AT WORK 2013 
+ * @copyright  MEN AT WORK 2013
  * @package    clipboard
- * @license    GNU/LGPL 
+ * @license    GNU/LGPL
  * @filesource
  */
 
 /**
  * Palettes
  */
-foreach ($GLOBALS['TL_DCA']['tl_user']['palettes'] as $key => $row)
-{
-    if ($key == '__selector__')
-    {    
+foreach ($GLOBALS['TL_DCA']['tl_user']['palettes'] as $key => $row) {
+    if ($key == '__selector__') {
         continue;
     }
 
-    $arrPalettes = explode(";", $row);
+    $arrPalettes   = explode(";", $row);
     $arrPalettes[] = '{clipboard_legend},clipboard,clipboard_context';
-    
+
     $GLOBALS['TL_DCA']['tl_user']['palettes'][$key] = implode(";", $arrPalettes);
 }
 
 /**
  * Fields
  */
-$GLOBALS['TL_DCA']['tl_user']['fields']['clipboard'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_user']['clipboard'],
+$GLOBALS['TL_DCA']['tl_user']['fields']['clipboard'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_user']['clipboard'],
     'inputType' => 'checkbox',
-    'eval' => array('tl_class' => 'clr w50')
-);
+    'eval'      => ['tl_class' => 'clr w50'],
+    'sql'       => 'char(1) NOT NULL default \'\''
+];
 
-$GLOBALS['TL_DCA']['tl_user']['fields']['clipboard_context'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_user']['clipboard_context'],
+$GLOBALS['TL_DCA']['tl_user']['fields']['clipboard_context'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_user']['clipboard_context'],
     'inputType' => 'checkbox',
-    'eval' => array('tl_class' => 'w50')
-);
-?>
+    'eval'      => ['tl_class' => 'w50'],
+    'sql'       => 'char(1) NOT NULL default \'\''
+];
